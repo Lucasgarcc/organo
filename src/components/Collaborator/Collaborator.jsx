72132549a6971e71e3a0980c5a4c7e2e9dc3d1d6
Lucasgarcc <luuccasgarciia@gmail.com>
2024-12-17 17:07:30 -0300

    feat: :sparkles: Criada nova feature para deletar colaborador (em desenvolvimento). Refatoração completa do CSS geral de todos os componentes e modificações nos componentes App, Time e Collaborator.

--- src/components/Collaborator/Collaborator.jsx
+++ src/components/Collaborator/Collaborator.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { MdDelete } from "react-icons/md";
 import './Collaborator.css'
 
-const Collaborator = ({ name, image, alt, position, colorBackgroud}) => {
+const Collaborator = ({ id, name, image, alt, position, colorBackgroud, deleteCollaborator }) => {
 
   return (
-    <div className="collaborator">
+    <div className="collaborator" key={id}>
       <div className="header" style={{ backgroundColor: colorBackgroud }}
       >
         <figure >
@@ -14,6 +15,12 @@ const Collaborator = ({ name, image, alt, position, colorBackgroud}) => {
       <div className="footer">
         <h4>{name}</h4>
         <h5>{position}</h5>
+      </div>
+      <div className="delete"  >
+        <p>Deletar</p>
+        <i >
+          <MdDelete className="icon-delete" color={colorBackgroud} fontSize={27} marginTop={'.3rem'} onClick={deleteCollaborator} />
+        </i>
       </div>
     </div>
   );
--- src/components/Time/Time.jsx
+++ src/components/Time/Time.jsx
@@ -1,33 +1,32 @@
 import Collaborator from '../Collaborator/Collaborator';
 import './Time.css';
 
-const Time = ({ title, colorBackground, colorCard, collaborator = [] }) => {
-
+const Time = ({ title, colorBackground, colorCard, deleteCollaborator,collaborator = [] }) => {
+  const filteredCollaborators = collaborator.filter(
+    (colab) => colab.time === title
+  );
 
   return (
-   (collaborator.length > 0) &&
-    <section className='time' style={{backgroundColor: colorBackground}}>
-      <h3 style={{borderColor: colorCard}}>{title}</h3>
-      <div className='time-collaborators'>
-      {collaborator.length > 0 ? (
-        collaborator
-        .filter((colab) => colab.time === title)
-        .map((colab) => (
-          <Collaborator
-            key={colab.name}
-            name={colab.name}
-            image={colab.image}
-            alt={`Imagem de ${colab.name}`}
-            position={colab.position}
-            colorBackgroud={colorCard}
-          />
-        ))
-      ) :(      
-        <p>Nenhum colaborador neste time ainda.</p>
-      )}
-      </div>
-
-    </section>
+    filteredCollaborators.length > 0 && (
+      <section className="time" style={{ backgroundColor: colorBackground }}>
+        <h3 style={{ borderColor: colorCard }}>{title}</h3>
+        <div className="time-collaborators">
+          {filteredCollaborators.map((colab, index) => {
+            return (
+              <Collaborator
+                key={index} // Use o ID único
+                name={colab.name}
+                image={colab.image}
+                alt={`Imagem de ${colab.name}`}
+                position={colab.position}
+                colorBackgroud={colorCard}
+                deleteCollaborator={deleteCollaborator}
+              />
+            )}
+          )}
+        </div>
+      </section>
+    )
   );
 };
 
